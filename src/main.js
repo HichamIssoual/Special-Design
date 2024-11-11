8e645d6cb54dac463dc2d8a0e09dea05c6e6eaa5
hichamissoual <hichamissoual.one@gmail.com>
2024-11-11 09:46:37 +0100

    feat:save last backrgoundIamge in localStorage andshow it when user refresh the page or something else

--- src/main.js
+++ src/main.js
@@ -14,9 +14,15 @@ let imgs = [
 	"09.jpg", 
 	"10.jpg"
 	];
+// check if user have any image in local storage
+if(localStorage.getItem("img")){
+	landing.style.backgroundImage = localStorage.getItem("img");
+}
 // set interval for doing the operation of number of seconds
 setInterval(()=>{
 	let randIndex = Math.floor(Math.random() * imgs.length);
 	landing.style.backgroundImage = `url(./imgs/landing/${imgs[randIndex]})`;
-},3000)
+	// add image to local storage
+	window.localStorage.setItem("img", `url(./imgs/landing/${imgs[randIndex]})`)
+},8000)
 // end Random background Image of Landing page
